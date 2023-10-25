15f5f9c794ae0cf818d706cba8e3e37d5665fa6d
Axolabb <142301524+Axolabb@users.noreply.github.com>
2023-10-25 17:41:10 +0300

    Update mainn.js

--- js/mainn.js
+++ js/mainn.js
@@ -8,22 +8,25 @@ button.addEventListener(`click`, () => {
     container.classList.add(`cont--open`)
     bg.classList.add(`bg--active`)
     body.style.overflow = `hidden`;
+    body.style.paddingRight = '17px';
 })
 x.addEventListener(`click`, () => {
     container.classList.remove(`cont--open`)
     bg.classList.remove(`bg--active`)
     body.style.overflow = `visible`;
+    body.style.paddingRight = '0';
 })
 bg.addEventListener(`click`, () => {
     container.classList.remove(`cont--open`)
     bg.classList.remove(`bg--active`)
     body.style.overflow = `visible`;
+    body.style.paddingRight = '0';
 })
 document.addEventListener(`keydown`, event => {
     if(event.code === `Escape`){
         container.classList.remove(`cont--open`)
         bg.classList.remove(`bg--active`)
         body.style.overflow = `visible`;
-        console.log(event.code)
+        body.style.paddingRight = '0';
     }
 })
