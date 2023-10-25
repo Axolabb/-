7408a1f424a9a7c692a6729a24e5551bcd56b78f
Axolabb <142301524+Axolabb@users.noreply.github.com>
2023-10-25 17:25:44 +0300

    Update mainn.js

--- js/mainn.js
+++ js/mainn.js
@@ -1,13 +1,29 @@
 const button = document.querySelector('.button')
 const container = document.querySelector(`.container`)
 const x = document.querySelector('.x')
+const bg = document.querySelector(`.bg`)
 const body = document.querySelector(`.body`)
 
 button.addEventListener(`click`, () => {
     container.classList.add(`cont--open`)
-    body.classList.add(`black`)
+    bg.classList.add(`bg--active`)
+    body.style.overflow = `hidden`;
 })
 x.addEventListener(`click`, () => {
     container.classList.remove(`cont--open`)
-    body.classList.remove(`black`)
+    bg.classList.remove(`bg--active`)
+    body.style.overflow = `visible`;
+})
+bg.addEventListener(`click`, () => {
+    container.classList.remove(`cont--open`)
+    bg.classList.remove(`bg--active`)
+    body.style.overflow = `visible`;
+})
+document.addEventListener(`keydown`, event => {
+    if(event.code === `Escape`){
+        container.classList.remove(`cont--open`)
+        bg.classList.remove(`bg--active`)
+        body.style.overflow = `visible`;
+        console.log(event.code)
+    }
 })
